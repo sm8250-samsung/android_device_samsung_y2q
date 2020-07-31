cf0a6a9006b372fbbef5464d9e9ab716fb44bc14
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2020-07-31 22:21:22 +0200

    y2q: Initial skeleton tree
    Change-Id: Ia0898290841bcb0394d7c6d6334e917e2960a143

---
+++ device.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from samsung sm8250-common
+$(call inherit-product, device/samsung/sm8250-common/common.mk)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Inherit from vendor blobs
+$(call inherit-product-if-exists, vendor/samsung/y2q/y2q-vendor.mk)
