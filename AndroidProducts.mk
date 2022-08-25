1af264cb1cff596a6931c05ee0e36b1334f72091
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-08-25 11:11:16 +0300

    Initial skeleton device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_y2q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_y2q-user \
+    lineage_y2q-userdebug \
+    lineage_y2q-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/y2q
